6a4c9a1d4a9f987eb6840e1608a7fba8ece47dcd
Deniz <92399617+dcanguven@users.noreply.github.com>
2025-09-22 15:45:48 +0300

    Update app.py

--- app.py
+++ app.py
@@ -64,4 +64,6 @@ def run_filler():
     return redirect("/")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
